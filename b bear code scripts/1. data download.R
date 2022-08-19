5b93d287e1957efd819c0b4669cc20dad6114b4b
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-08-19 10:33:28 -0600

    correction

--- b bear code scripts/1. data download.R
+++ b bear code scripts/1. data download.R
@@ -44,7 +44,7 @@ folder <- drive_get(as_id(folder_url))
 gdrive_files <- drive_ls(folder)
 #have to treat the gdb as a folder and download it into a gdb directory in order to deal with the fact that gdb is multiple, linked files
 lapply(gdrive_files$id, function(x) drive_download(as_id(x),
-                                                   path = paste0(here::here("data/original/forest_inventory.gdb/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
+                                                   path = paste0(here::here("data/original/Forestinventory.gdb/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
 
 
 # gHM:
