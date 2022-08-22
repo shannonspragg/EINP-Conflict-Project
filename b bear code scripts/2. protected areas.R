80ec87d2977c8f7ae5580a33b314dd469f6d7081
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-08-22 12:16:02 -0600

    scripts

--- b bear code scripts/2. protected areas.R
+++ b bear code scripts/2. protected areas.R
@@ -1,4 +1,62 @@
 
-# Prep Downloaded HSI Data ----------------------------------------------------
-  # Filter environment variables down to our study area 
+# Prep PA Data ----------------------------------------------------
+  # Filter to AB and IUCN status
+
+
+# Load Packages -----------------------------------------------------------
+library(tidyverse)
+library(sf)
+library(rgdal)
+library(terra)
+library(gdalUtilities)
+
+# Load BC PAs -------------------------------------------------------------
+fgdb <- "data/original/CPCAD-BDCAPC_Dec2020.gdb"
+fc <- readOGR(dsn=fgdb,layer="CPCAD_Dec2020")
+fc.sf <- as(fc, "sf")
+ab.PAs <- fc.sf %>% 
+  filter(., LOC_E == "Alberta") 
+
+# Filter by IUCN status (Muise et al., 2022 https://esajournals.onlinelibrary.wiley.com/doi/10.1002/eap.2603)
+ab.PAs.iucn.filtered <- ab.PAs %>% 
+  filter(., IUCN_CAT == "Ia" | IUCN_CAT == "Ib" | IUCN_CAT == "II" | IUCN_CAT == "IV") %>% 
+  st_make_valid()
+ab.PAs.iucn.filtered$areaha <- st_area(ab.PAs.iucn.filtered) 
+units(ab.PAs.iucn.filtered$areaha) <- units::make_units(ha)
+ab.PAs.iucn.filtered$areaha <- as.numeric(ab.PAs.iucn.filtered$areaha) 
+
+# Filter by PA's larger than 100 ha:
+ab.PAs.fin <- filter(ab.PAs.iucn.filtered, areaha > 100) 
+
+
+# create template raster --------------------------------------------------
+
+  # Bring in land use regions:
+ab.regions <- st_read("data/original/LUF Integrated Regional Plan Boundaries.shp")
+ab.crs <- st_crs
+n.sask <- ab.regions %>%
+  filter(., LUF_NAME == "North Saskatchewan")
+
+n.sask.proj <- n.sask %>% st_set_crs(3400) %>% st_transform(3400)
+
+n.sask.bound <- n.sask.proj %>%
+  st_buffer(., 25000) 
+
+n.sask.bound.v <- n.sask.bound %>% as(., "SpatVector")
+
+st_write(n.sask, "data/processed/north_saskatchewan.shp", append= FALSE)
+st_write(n.sask.bound, "data/processed/north_saskatchewan_25km.shp", append=FALSE)
+
+temp.rast <- rast(res=c(1000,1000), ext=ext(n.sask.bound))
+crs(temp.rast) <- crs(n.sask.bound)
+values(temp.rast) <- rep(1, ncell(temp.rast))
+
+ab.pa.proj <- ab.PAs.fin %>% 
+  st_transform(., crs=crs(temp.rast)) %>%
+  as(., "SpatVector")
+
+  # Dist to PA raster:
+dist2pa <- terra::distance(temp.rast, ab.pa.proj)
+dist2pa.km <- measurements::conv_unit(dist2pa, "m", "km")
+writeRaster(dist2pa.km, "data/processed/dist2pa_km_n_sasck.tif", overwrite=TRUE)
 
---
+++ b bear code scripts/3. wildfire history.R
@@ -0,0 +1,30 @@
+# Prep Wildfire Data ----------------------------------------------------
+# Filter to burned areas within the last 20 years
+
+
+# Load Packages -----------------------------------------------------------
+library(tidyverse)
+library(sf)
+library(rgdal)
+library(terra)
+library(gdalUtilities)
+
+
+# Filter wildfires by year ------------------------------------------------
+historic_wildfires <- st_read("data/original/WildfirePerimeters1931to2021v2.shp")
+
+recent_wildfires <- historic_wildfires %>% filter(historic_wildfires$YEAR >= 2003) # Filter to last 20 years
+plot(recent_wildfires['FIRE_NUMBE'])
+
+
+# Crop to our Region --------------------------------------------------------
+north.sask.bound <- st_read("data/processed/north_saskatchewan_25km.shp")
+
+st_crs(recent_wildfires) == st_crs(north.sask.bound)
+st_make_valid(recent_wildfires)
+st_make_valid(north.sask.bound)
+
+n.sask.b.v <- vect(north.sask.bound)
+wildfires.v <- vect(recent_wildfires)
+
+ns.fires <- terra::crop(wildfires.v, n.sask.b.v)
