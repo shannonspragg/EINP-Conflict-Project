8329b87887e2656907f80f31690df4870f961dcf
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-08-31 21:52:30 -0600

    trying to figure out aspect categories

--- black bear analyses/1.  b bear habitat data scripts/elev and terrain.R
+++ black bear analyses/1.  b bear habitat data scripts/elev and terrain.R
@@ -22,8 +22,24 @@ slope.bhb <- raster::terrain(elev.bhb.raster, opt = "slope", unit='degrees')
 aspect.bhb <- raster::terrain(elev.bhb.raster, opt = "aspect", unit='degrees')
 
 # Code aspect into categories: flat, N, NE, E, SE, S, SW, W, NW;
+
 aspect.flat <- (aspect.bhb == -1)
-aspect.N <- (aspect.bhb < 22.5)
+aspect.NW <- (aspect.bhb <= 22.5)
+
+aspect.N <- aspect.bhb[0,22.5]
+aspect.NE <- (aspect.bhb == 22.5:67.5 )
+
+#Reclassify the values into 7 groups
+#all values between 0 and 20 equal 1, etc.
+m <- c(-1, 0, 22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5, 360)
+
+r <- as.factor(aspect.bhb)
+
+## Add a landcover column to the Raster Attribute Table
+rat <- levels(r)[[1]]
+rat[["aspect direction"]] <- c("flat","N", "NE","E", "SE", "S", "SW", "W", "NW")
+levels(r) <- rat
+
 
 # is.factor(aspect.bhb)
 # direction <- data.frame(ID=1:9, cover=c("flat", "N", "NE", "E", "SE", "S", "SW", "W", "NW"))
