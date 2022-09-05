65032a478236a622e5259706eb477b7d402f66bb
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-09-05 14:46:12 -0600

    running elevation for 50km

--- black bear analyses/1.  b bear habitat data scripts/elev and terrain.R
+++ black bear analyses/1.  b bear habitat data scripts/elev and terrain.R
@@ -9,9 +9,9 @@ library(tidyverse)
 library(raster)
 
 # Bring in Data -----------------------------------------------------------
-bhb.10km.boundary <- st_read("data/processed/bhb_10km.shp")
+bhb.50km.boundary <- st_read("data/processed/bhb_50km.shp")
 temp.rast <- rast("data/processed/dist2pa_km_bhb.tif")
-bhb.bound.v <- vect(bhb.10km.boundary)
+bhb.bound.v <- vect(bhb.50km.boundary)
 
 # Prep elevation data: ----------------------------------------------------
 elev.can <- rast(raster::getData('alt', country = 'CAN'))
@@ -19,28 +19,28 @@ elev.bhb.crop <- crop(elev.can, project(bhb.bound.v, elev.can)) #crop to bhb
 
 elev.bhb.raster <- raster(elev.bhb.crop)
 slope.bhb <- raster::terrain(elev.bhb.raster, opt = "slope", unit='degrees')
-aspect.bhb <- raster::terrain(elev.bhb.raster, opt = "aspect", unit='degrees')
+#aspect.bhb <- raster::terrain(elev.bhb.raster, opt = "aspect", unit='degrees')
 
 
 # Code aspect into categories: flat, N, NE, E, SE, S, SW, W, NW;
-
-aspect.flat <- (aspect.bhb == -1)
-aspect.NW <- (aspect.bhb <= 22.5)
-
-aspect.N <- aspect.bhb[0,22.5]
-aspect.NE <- (aspect.bhb == 22.5:67.5 )
-
-#Reclassify the values into 7 groups
-#all values between 0 and 20 equal 1, etc.
-m <- c(-1, 0, 22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5, 360)
-
-r <- as.factor(aspect.bhb)
-
-## Add a landcover column to the Raster Attribute Table
-rat <- levels(r)[[1]]
-rat[["aspect direction"]] <- c("flat","N", "NE","E", "SE", "S", "SW", "W", "NW")
-levels(r) <- rat
-
+# 
+# aspect.flat <- (aspect.bhb == -1)
+# aspect.NW <- (aspect.bhb <= 22.5)
+# 
+# aspect.N <- aspect.bhb[0,22.5]
+# aspect.NE <- (aspect.bhb == 22.5:67.5 )
+# 
+# #Reclassify the values into 7 groups
+# #all values between 0 and 20 equal 1, etc.
+# m <- c(-1, 0, 22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5, 360)
+# 
+# r <- as.factor(aspect.bhb)
+# 
+# ## Add a landcover column to the Raster Attribute Table
+# rat <- levels(r)[[1]]
+# rat[["aspect direction"]] <- c("flat","N", "NE","E", "SE", "S", "SW", "W", "NW")
+# levels(r) <- rat
+# 
 
 # is.factor(aspect.bhb)
 # direction <- data.frame(ID=1:9, cover=c("flat", "N", "NE", "E", "SE", "S", "SW", "W", "NW"))
