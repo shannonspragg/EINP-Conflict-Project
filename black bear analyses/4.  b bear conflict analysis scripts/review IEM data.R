8c2ae442178162d6d1a256f9323c622f96282b55
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-10-12 15:56:16 -0600

    new script

---
+++ black bear analyses/4.  b bear conflict analysis scripts/review IEM data.R
@@ -0,0 +1,7 @@
+
+# Review IEM Data: --------------------------------------------------------
+    ## Here we bring in the IEM conflict data and review it for usability. We need to cross reference it with the
+    # province conflict records in order to rule out and possible duplicate reports.
+
+
+
