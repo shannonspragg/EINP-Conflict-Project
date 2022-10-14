cf3db29b2f33f84313dfb0640059c7b86b1bb4e9
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-10-14 12:00:26 -0600

    adding full dataset

--- black bear analyses/4.  b bear conflict analysis scripts/Testing Cat's data.R
+++ black bear analyses/4.  b bear conflict analysis scripts/Testing Cat's data.R
@@ -20,29 +20,46 @@ ab.pas <- st_read("data/processed/alberta_protected_areas.shp")
 head(ct.reports)
 unique(ct.reports$Dataset)
 
-
+# Look at only CT reports
 ct_only <- ct.reports %>% filter(ct.reports$Dataset == "camera_trap_predators")
 ct_filtered <- ct_only %>% 
   dplyr::select(., c('FID', 'Dataset', 'Species', 'Latitude', 'Longitude', 'Year'))
 head(ct_filtered)
 
 
-# Make soatial:
+# Make spatial:
 
 xy.ct<-ct_filtered[,c(5,4)]
 ct.spdf<-SpatialPointsDataFrame(coords = xy.ct,data = ct_filtered,
                                       proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
 
+# Drop reports with NA locations:
+ct.no.na <- ct.reports[complete.cases(ct.reports[,c("Latitude", "Longitude")]),] # This leaves us 9759 total obs
+
+xy.cd<-ct.no.na[,c(5,4)]
+cd.spdf<-SpatialPointsDataFrame(coords = xy.cd,data = ct.no.na,
+                                proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+
 # Ensure this is a sf data frame:
 # conflict.data.sf <- as(conflict.spdf, "sf")
 ct.data.sf <- st_as_sf(ct.spdf)
+ct.all.sf <- st_as_sf(cd.spdf) # this includes all misc reports in the file
 
 head(ct.data.sf)
 ct.reproj <- st_transform(ct.data.sf, st_crs(bhb.50k.buf))
+cd.reproj <- st_transform(ct.all.sf, st_crs(bhb.50k.buf))
+
 ab.pa.reproj <- st_transform(ab.pas, st_crs(bhb.50k.buf))
 
-
+# Plot just the CT locations:
 plot(st_geometry(ab.pa.reproj))
 plot(st_geometry(ct.reproj), add=TRUE, col="red") 
 # These locations appear to be all over Alberta - which is not what we want.
 # Either these CT reports are sourced from elsewhere or the coordinates are wrong.
+
+# Plot all report locations:
+plot(st_geometry(ab.pa.reproj))
+plot(st_geometry(cd.reproj), add=TRUE, col="red") 
+
+plot(st_geometry(bhb.50k.buf)) # zoom in
+plot(st_geometry(cd.reproj), add=TRUE, col="red") # decent amount in / around the park - from conflict reports (I think)
