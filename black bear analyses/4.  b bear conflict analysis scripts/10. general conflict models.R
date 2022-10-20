ed43dcf7e4d459bde79050f9d91005f31abfe2f3
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-10-20 10:12:04 -0600

    setting up for full model run

--- black bear analyses/4.  b bear conflict analysis scripts/10. general conflict models.R
+++ black bear analyses/4.  b bear conflict analysis scripts/10. general conflict models.R
@@ -19,29 +19,32 @@ theme_set(bayesplot::theme_default(base_family = "sans"))
 
 
 # Bring in Data: ----------------------------------------------------------
-warp.pres.abs <- st_read(here::here("./Data/processed/pres_abs_full_df.shp")) %>% st_drop_geometry()
+conf.pres.abs <- st_read(here::here("./Data/processed/pres_abs_full_df.shp")) %>% st_drop_geometry()
+
+# Add a column for conflict presence:
+conf.pres.abs$conflict_presence_pa <- 1
 
 #filter some of the absences
-pres.abs.filter <- warp.pres.abs %>%
-  filter(anyCnfl == 0) %>%
-  slice_sample(n = 9000) %>%
-  bind_rows(warp.pres.abs %>% filter(anyCnfl != 0)) %>% 
-  dplyr::select(., c(anyCnfl, dst__PA, dst_t_M, Anml_Fr, Grnd_Cr, CCSNAME, Human_Dens)) 
+pres.abs.filter <- conf.pres.abs %>% 
+  dplyr::select(., c(conflict_presence_pa, dst2p_k, hum_dns, anml_fr, grnd_cr, ndvi, gHM, bhs, agn_bph, gfcl_bp, frst_s_, CCSNAME)) 
 
-colnames(pres.abs.filter) <- c("conflict_presence_ps", "dist.2.pa.ps", "dist.2.met.ps", "animal.farm.dens.ps", "ground.crop.dens.ps", "CCSNAME.ps", "pop.dens")
+colnames(pres.abs.filter) <- c("conflict_presence_ps", "dist.2.pa.ps", "human.dens.ps", "animal.farm.dens.ps", "ground.crop.dens.ps", "ndvi.ps", 
+                               "gHM.ps", "bhs.ps", "agno.biophys.ps", "gen.focal.biophys.ps", "forest.sp.biophys.ps",  "CCSNAME.ps")
 
 
 # Scale Data for Analysis: ------------------------------------------------
 ## Here we scale the predictors for analysis
 pres.abs.scl <- pres.abs.filter %>% 
-  mutate_at(c("dist.2.pa.ps", "dist.2.met.ps", "animal.farm.dens.ps", "ground.crop.dens.ps",  "pop.dens"), scale)
+  mutate_at(c("dist.2.pa.ps", "human.dens.ps", "animal.farm.dens.ps", "ground.crop.dens.ps", "ndvi.ps", 
+              "gHM.ps", "bhs.ps", "agno.biophys.ps", "gen.focal.biophys.ps", "forest.sp.biophys.ps"), scale)
 
 
 # Run General Conflict Models: --------------------------------------------
 t_prior <- student_t(df = 7, location = 0, scale = 1.5)
 SEED<-14124869
+
 # Full Model:
-post.pa.full <- stan_glmer(conflict_presence_ps ~ dist.2.pa.ps + dist.2.met.ps + animal.farm.dens.ps + ground.crop.dens.ps + pop.dens + (1 | CCSNAME.ps), 
+post.pa.full <- stan_glmer(conflict_presence_ps ~ dist.2.pa.ps + human.dens.ps + animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + bhs.ps + agno.biophys.ps + gen.focal.biophys.ps + forest.sp.biophys.ps + (1 | CCSNAME.ps), 
                            data = pres.abs.scl,
                            family = binomial(link = "logit"), # define our binomial glm
                            prior = t_prior, prior_intercept = t_prior, QR=TRUE,
