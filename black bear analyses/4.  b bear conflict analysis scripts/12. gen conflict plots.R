ff13aacd833d1d0c43aa9eb0503e51694c98a5cf
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-10-21 14:00:27 -0600

    making plots!

--- black bear analyses/4.  b bear conflict analysis scripts/12. gen conflict plots.R
+++ black bear analyses/4.  b bear conflict analysis scripts/12. gen conflict plots.R
@@ -23,10 +23,7 @@ library(ggplot2)
 
 
 # Bring in Data: ----------------------------------------------------------
-post.pa.full <- readRDS("Data/processed/post_pa_full.rds")
-post.pa.full.quad <- readRDS("Data/processed/post_pa_full_quad.rds")
-post.int.only <- readRDS("Data/processed/post_int_only.rds")
-
+post.pa.full <- readRDS("data/processed/post_pa_full.rds")
 
 # Plot Effects of Posterior Coefficients:
 
@@ -34,43 +31,79 @@ post.pa.result <- p_direction(post.pa.full)
 post.pa.full.preds.plot <- plot(post.pa.result, title = "Predictor Effects for General Wildlife Conflict")
 # this is the max probability of effect (MPE), showing the probability of a predictor having a positive or negative effect
 
-gen_conf_coef_plot <- plot(post.pa.full, pars = c("dist.2.pa.ps","dist.2.met.ps",
+gen_conf_coef_plot <- plot(post.pa.full, pars = c("dist.2.pa.ps","human.dens.ps",
                                                   "animal.farm.dens.ps",
                                                   "ground.crop.dens.ps",
-                                                  "pop.dens"), main = "Predictor Effects for General Wildlife Conflict")
+                                                  "ndvi.ps", "gHM.ps", "agno.biophys.ps"), main = "Predictor Effects for General Wildlife Conflict")
 
-saveRDS(post.pa.full.preds.plot, "Data/processed/post_pa_full_predsplot.rds")
-saveRDS(gen_conf_coef_plot, "Data/processed/post_pa_coef_plot.rds")
+saveRDS(post.pa.full.preds.plot, "data/processed/post_pa_full_predsplot.rds")
+saveRDS(gen_conf_coef_plot, "data/processed/post_pa_coef_plot.rds")
 
 # Simulate Data & Posterior Predictive Draws: -----------------------------
 
 posterior <- as.matrix(post.pa.full)
 p <- mcmc_intervals(posterior,
                     pars = c("dist.2.pa.ps",
-                             "dist.2.met.ps",
+                             "human.dens.ps",
                              "animal.farm.dens.ps",
                              "ground.crop.dens.ps",
-                             "pop.dens"),
+                             "ndvi.ps", "gHM.ps", "agno.biophys.ps"),
                     prob = 0.8) +
   scale_y_discrete(labels = c("dist.2.pa.ps" = "Dist. to PA",
-                              "dist.2.met.ps" = "Dist. to metro",
+                              "human.dens.ps" = "Population Density",
                               "animal.farm.dens.ps" = "Dens. of livestock ops.",
                               "ground.crop.dens.ps" = "Dens. of row-crop ops.",
-                              "pop.dens" = "Population dens.")) 
+                              "ndvi.ps" = "NDVI", "gHM.ps" = "Human Modification", "agno.biophys.ps" = "Agnostic Biophys. Connectivity")) 
 
+# Prep plot for Dist 2 PAs:
 
+simdata <- pres.abs.scl %>%
+  modelr::data_grid(dist.2.pa.ps = seq_range(dist.2.pa.ps, n=300),
+                    human.dens.ps = quantile(pres.abs.scl$human.dens.ps, probs = c(0.1, 0.5, 0.9)),
+                    animal.farm.dens.ps = mean(animal.farm.dens.ps),
+                    ground.crop.dens.ps = mean(ground.crop.dens.ps),
+                    ndvi.ps = mean(ndvi.ps),
+                    gHM.ps = mean(gHM.ps),
+                    agno.biophys.ps = mean(agno.biophys.ps))
+
+postdraws <- tidybayes::add_epred_draws(post.pa.full, 
+                                        newdata=simdata,
+                                        ndraws=1000,
+                                        re_formula=NA)
+
+postdraws$dist.2.pa.ps <- (postdraws$dist.2.pa.ps * attributes(pres.abs.scl$dist.2.pa.ps)[[3]])+attributes(pres.abs.scl$dist.2.pa.ps)[[2]]
+
+# Plotting Dist2PA's: # NOTE: changing .value to .epred to match update from add_epred_draws
+plot.df <- postdraws %>% 
+  mutate_at(., vars(human.dens.ps), as.factor) %>% 
+  group_by(dist.2.pa.ps, human.dens.ps) %>% 
+  dplyr::summarise(., mean = mean(.epred),
+                   lo = quantile(.epred, 0.2),
+                   hi = quantile(.epred, 0.8))
+
+levels(plot.df$human.dens.ps) <-  c("Lower 10%", "Mean", "Upper 10%")
+dist2pa.plot <- ggplot(data=plot.df) +
+  geom_line(aes(x = dist.2.pa.ps, y = mean, colour =human.dens.ps), lwd=1.5) +
+  geom_ribbon(aes(ymin=lo, ymax=hi, x=dist.2.pa.ps, fill = human.dens.ps), alpha = 0.2) +
+  scale_colour_viridis(discrete = "TRUE", option="D","Population Density")+
+  scale_fill_viridis(discrete = "TRUE", option="D", "Population Density") +
+  ylab("Probability of Conflict") + 
+  xlab(expression("Distance to Protected Areas (km)"))+
+  # guides(fill=guide_legend(title="Population Density"))+
+  theme(text=element_text(size=12,  family="Times New Roman"), legend.text = element_text(size=10),panel.background = element_rect(fill = "white", colour = "grey50"))
+
+saveRDS(dist2pa.plot, "data/processed/dist2pa_mixe_plot.rds")
+
+# Prep plot for Livestock density:
 
 simdata <- pres.abs.scl %>%
   modelr::data_grid(dist.2.pa.ps = mean(dist.2.pa.ps),
-                    dist.2.met.ps = mean(dist.2.met.ps),
+                    human.dens.ps = quantile(pres.abs.scl$human.dens.ps, probs = c(0.1, 0.5, 0.9)),
                     animal.farm.dens.ps = seq_range(animal.farm.dens.ps, n=300),
                     ground.crop.dens.ps = mean(ground.crop.dens.ps),
-                    pop.dens = quantile(pres.abs.scl$pop.dens, probs = c(0.1, 0.5, 0.9)))
-
-# postdraws <- tidybayes::add_fitted_draws(post.pa.full, 
-#                                  newdata=simdata,
-#                                  ndraws=1000,
-#                                  re_formula=NA)
+                    ndvi.ps = mean(ndvi.ps),
+                    gHM.ps = mean(gHM.ps),
+                    agno.biophys.ps = mean(agno.biophys.ps))
 
 postdraws <- tidybayes::add_epred_draws(post.pa.full, 
                                         newdata=simdata,
@@ -81,22 +114,24 @@ postdraws$animal.farm.dens <- (postdraws$animal.farm.dens.ps * attributes(pres.a
 
 # Plotting Livestock Density: # NOTE: changing .value to .epred to match update from add_epred_draws
 plot.df <- postdraws %>% 
-  mutate_at(., vars(pop.dens), as.factor) %>% 
-  group_by(animal.farm.dens, pop.dens) %>% 
+  mutate_at(., vars(human.dens.ps), as.factor) %>% 
+  group_by(animal.farm.dens, human.dens.ps) %>% 
   dplyr::summarise(., mean = mean(.epred),
                    lo = quantile(.epred, 0.2),
                    hi = quantile(.epred, 0.8))
 
-levels(plot.df$pop.dens) <-  c("Lower 10%", "Mean", "Upper 10%")
+levels(plot.df$human.dens.ps) <-  c("Lower 10%", "Mean", "Upper 10%")
 animal.dens.plot <- ggplot(data=plot.df) +
-  geom_line(aes(x = animal.farm.dens, y = mean, colour =pop.dens), lwd=1.5) +
-  geom_ribbon(aes(ymin=lo, ymax=hi, x=animal.farm.dens, fill = pop.dens), alpha = 0.2) +
+  geom_line(aes(x = animal.farm.dens, y = mean, colour =human.dens.ps), lwd=1.5) +
+  geom_ribbon(aes(ymin=lo, ymax=hi, x=animal.farm.dens, fill = human.dens.ps), alpha = 0.2) +
   scale_colour_viridis(discrete = "TRUE", option="D","Population Density")+
   scale_fill_viridis(discrete = "TRUE", option="D", "Population Density") +
   ylab("Probability of Conflict") + 
   xlab(expression("Density of Livestock Operations per"~km^{2}))+
   # guides(fill=guide_legend(title="Population Density"))+
   theme(text=element_text(size=12,  family="Times New Roman"), legend.text = element_text(size=10),panel.background = element_rect(fill = "white", colour = "grey50"))
+
+saveRDS(animal.dens.plot, "data/processed/animal_dens_mixe_plot.rds")
 
 ###### Testing other color palette:
 # library(RColorBrewer)
@@ -112,13 +147,16 @@ animal.dens.plot <- ggplot(data=plot.df) +
 #   # guides(fill=guide_legend(title="Population Density"))+
 #   theme(text=element_text(size=12,  family="Times New Roman"), legend.text = element_text(size=10),panel.background = element_rect(fill = "white", colour = "grey50"))
 
+# Prep plot for Row crop density:
 
 simdata <- pres.abs.scl %>%
   modelr::data_grid(dist.2.pa.ps = mean(dist.2.pa.ps),
-                    dist.2.met.ps = mean(dist.2.met.ps),
+                    human.dens.ps = quantile(pres.abs.scl$human.dens.ps, probs = c(0.1, 0.5, 0.9)),
                     animal.farm.dens.ps = mean(animal.farm.dens.ps),
-                    ground.crop.dens.ps = seq_range(ground.crop.dens.ps,n=300),
-                    pop.dens = quantile(pres.abs.scl$pop.dens, probs = c(0.1, 0.5, 0.9)))
+                    ground.crop.dens.ps = seq_range(ground.crop.dens.ps, n=300),
+                    ndvi.ps = mean(ndvi.ps),
+                    gHM.ps = mean(gHM.ps),
+                    agno.biophys.ps = mean(agno.biophys.ps))
 
 postdraws <- tidybayes::add_epred_draws(post.pa.full, 
                                         newdata=simdata,
@@ -129,91 +167,137 @@ postdraws$ground.crop.dens <- (postdraws$ground.crop.dens.ps * attributes(pres.a
 
 # Plotting Row Crop Dens:
 plot.df <- postdraws %>% 
-  mutate_at(., vars(pop.dens), as.factor) %>% 
-  group_by(ground.crop.dens, pop.dens) %>% 
+  mutate_at(., vars(human.dens.ps), as.factor) %>% 
+  group_by(ground.crop.dens, human.dens.ps) %>% 
   summarise(., mean = mean(.epred),
             lo = quantile(.epred, 0.2),
             hi = quantile(.epred, 0.8))
 
-levels(plot.df$pop.dens) <-  c("Lower 10%", "Mean", "Upper 10%")
+levels(plot.df$human.dens.ps) <-  c("Lower 10%", "Mean", "Upper 10%")
 ground.dens.plot <- ggplot(data=plot.df) +
-  geom_line(aes(x = ground.crop.dens, y = mean, colour =pop.dens), lwd=1.5) +
-  geom_ribbon(aes(ymin=lo, ymax=hi, x=ground.crop.dens, fill = pop.dens), alpha = 0.2) +
+  geom_line(aes(x = ground.crop.dens, y = mean, colour =human.dens.ps), lwd=1.5) +
+  geom_ribbon(aes(ymin=lo, ymax=hi, x=ground.crop.dens, fill = human.dens.ps), alpha = 0.2) +
   scale_colour_viridis(discrete = "TRUE", option="D","Population Density")+
   scale_fill_viridis(discrete = "TRUE", option="D", "Population Density") +
   ylab("Probability of Conflict") + 
   xlab(expression("Density of Row-Crop Operations per"~km^{2}))+
   # guides(fill=guide_legend(title="Population Density"))+
   theme(text=element_text(size=12,  family="Times New Roman"), legend.text = element_text(size=10),panel.background = element_rect(fill = "white", colour = "grey50"))
+saveRDS(ground.dens.plot, "data/processed/groundcrop_dens_mixe_plot.rds")
 
-
+# Prep plot for NDVI:
 simdata <- pres.abs.scl %>%
   modelr::data_grid(dist.2.pa.ps = mean(dist.2.pa.ps),
-                    dist.2.met.ps = seq_range(dist.2.met.ps, n=300),
+                    human.dens.ps = quantile(pres.abs.scl$human.dens.ps, probs = c(0.1, 0.5, 0.9)),
                     animal.farm.dens.ps = mean(animal.farm.dens.ps),
                     ground.crop.dens.ps = mean(ground.crop.dens.ps),
-                    pop.dens = quantile(pres.abs.scl$pop.dens, probs = c(0.1, 0.5, 0.9)))
+                    ndvi.ps = seq_range(ndvi.ps, n=300),
+                    gHM.ps = mean(gHM.ps),
+                    agno.biophys.ps = mean(agno.biophys.ps))
 
 postdraws <- tidybayes::add_epred_draws(post.pa.full, 
                                         newdata=simdata,
                                         ndraws=1000,
                                         re_formula=NA)
 
-postdraws$dist.2.met <- (postdraws$dist.2.met.ps * attributes(pres.abs.scl$dist.2.met.ps)[[3]])+attributes(pres.abs.scl$dist.2.met.ps)[[2]]
+postdraws$ndvi.ps <- (postdraws$ndvi.ps * attributes(pres.abs.scl$ndvi.ps)[[3]])+attributes(pres.abs.scl$ndvi.ps)[[2]]
 
 # Plot Dist 2 Metro Areas:
 plot.df <- postdraws %>% 
-  mutate_at(., vars(pop.dens), as.factor) %>% 
-  group_by(dist.2.met, pop.dens) %>% 
+  mutate_at(., vars(human.dens.ps), as.factor) %>% 
+  group_by(ndvi.ps, human.dens.ps) %>% 
   summarise(., mean = mean(.epred),
             lo = quantile(.epred, 0.2),
             hi = quantile(.epred, 0.8))
 
-levels(plot.df$pop.dens) <-  c("Lower 10%", "Mean", "Upper 10%")
-dist.2met.plot <- ggplot(data=plot.df) +
-  geom_line(aes(x = dist.2.met, y = mean, colour =pop.dens), lwd=1.5) +
-  geom_ribbon(aes(ymin=lo, ymax=hi, x=dist.2.met, fill = pop.dens), alpha = 0.2) +
+levels(plot.df$human.dens.ps) <-  c("Lower 10%", "Mean", "Upper 10%")
+ndvi.plot <- ggplot(data=plot.df) +
+  geom_line(aes(x = ndvi.ps, y = mean, colour =human.dens.ps), lwd=1.5) +
+  geom_ribbon(aes(ymin=lo, ymax=hi, x=ndvi.ps, fill = human.dens.ps), alpha = 0.2) +
   scale_colour_viridis(discrete = "TRUE", option="D","Population Density")+
   scale_fill_viridis(discrete = "TRUE", option="D", "Population Density") +
   ylab("Probability of Conflict") + 
-  xlab("Distance to Metro Areas (km)")+
+  xlab("Normalized Difference Vegetation Index (NDVI)")+
   # guides(fill=guide_legend(title="Population Density"))+
   theme(text=element_text(size=12,  family="Times New Roman"), legend.text = element_text(size=10),panel.background = element_rect(fill = "white", colour = "grey50"))
+saveRDS(ndvi.plot, "data/processed/ndvi_mixe_plot.rds")
 
+# Prep gHM plot:
 simdata <- pres.abs.scl %>%
-  modelr::data_grid(dist.2.pa.ps = seq_range(dist.2.pa.ps, n=300),
-                    dist.2.met.ps = mean(dist.2.met.ps),
+  modelr::data_grid(dist.2.pa.ps = mean(dist.2.pa.ps),
+                    human.dens.ps = quantile(pres.abs.scl$human.dens.ps, probs = c(0.1, 0.5, 0.9)),
                     animal.farm.dens.ps = mean(animal.farm.dens.ps),
                     ground.crop.dens.ps = mean(ground.crop.dens.ps),
-                    pop.dens = quantile(pres.abs.scl$pop.dens, probs = c(0.1, 0.5, 0.9)))
+                    ndvi.ps = mean(ndvi.ps),
+                    gHM.ps = seq_range(gHM.ps, n=300),
+                    agno.biophys.ps = mean(agno.biophys.ps))
 
 postdraws <- tidybayes::add_epred_draws(post.pa.full, 
                                         newdata=simdata,
                                         ndraws=1000,
                                         re_formula=NA)
 
-postdraws$dist.2.pa <- (postdraws$dist.2.pa.ps * attributes(pres.abs.scl$dist.2.pa.ps)[[3]])+attributes(pres.abs.scl$dist.2.pa.ps)[[2]]
+postdraws$gHM.ps <- (postdraws$gHM.ps * attributes(pres.abs.scl$gHM.ps)[[3]])+attributes(pres.abs.scl$gHM.ps)[[2]]
 
 # Plotting Dist 2 PA's:
 plot.df <- postdraws %>% 
-  mutate_at(., vars(pop.dens), as.factor) %>% 
-  group_by(dist.2.pa, pop.dens) %>% 
+  mutate_at(., vars(human.dens.ps), as.factor) %>% 
+  group_by(gHM.ps, human.dens.ps) %>% 
   summarise(., mean = mean(.epred),
             lo = quantile(.epred, 0.2),
             hi = quantile(.epred, 0.8))
 
-levels(plot.df$pop.dens) <-  c("Lower 10%", "Mean", "Upper 10%")
-dist.2pa.plot <- ggplot(data=plot.df) +
-  geom_line(aes(x = dist.2.pa, y = mean, colour =pop.dens), lwd=1.5) +
-  geom_ribbon(aes(ymin=lo, ymax=hi, x=dist.2.pa, fill = pop.dens), alpha = 0.2) +
+levels(plot.df$human.dens.ps) <-  c("Lower 10%", "Mean", "Upper 10%")
+gHM.plot <- ggplot(data=plot.df) +
+  geom_line(aes(x = gHM.ps, y = mean, colour =human.dens.ps), lwd=1.5) +
+  geom_ribbon(aes(ymin=lo, ymax=hi, x=gHM.ps, fill = human.dens.ps), alpha = 0.2) +
   scale_colour_viridis(discrete = "TRUE", option="D","Population Density")+
   scale_fill_viridis(discrete = "TRUE", option="D", "Population Density") +
   ylab("Probability of Conflict") + 
-  xlab("Distance to Protected Area (km)")+
+  xlab("Global Human Modification (gHM)")+
   # guides(fill=guide_legend(title="Population Density"))+
   theme(text=element_text(size=12,  family="Times New Roman"), legend.text = element_text(size=10),panel.background = element_rect(fill = "white", colour = "grey50"))
+saveRDS(gHM.plot, "data/processed/gHM_mixe_plot.rds")
 
-p.all <- animal.dens.plot + ground.dens.plot + dist.2met.plot + dist.2pa.plot + plot_annotation(tag_levels = 'a', tag_suffix = ")") +  plot_layout(guides = 'collect')
+# Prep Biophysical connectivity plot:
+simdata <- pres.abs.scl %>%
+  modelr::data_grid(dist.2.pa.ps = mean(dist.2.pa.ps),
+                    human.dens.ps = quantile(pres.abs.scl$human.dens.ps, probs = c(0.1, 0.5, 0.9)),
+                    animal.farm.dens.ps = mean(animal.farm.dens.ps),
+                    ground.crop.dens.ps = mean(ground.crop.dens.ps),
+                    ndvi.ps = mean(ndvi.ps),
+                    gHM.ps = mean(gHM.ps),
+                    agno.biophys.ps = seq_range(agno.biophys.ps, n=300))
 
-saveRDS(p.all, "Data/processed/general_conflict_all_pred_plot.rds")
+postdraws <- tidybayes::add_epred_draws(post.pa.full, 
+                                        newdata=simdata,
+                                        ndraws=1000,
+                                        re_formula=NA)
+
+postdraws$agno.biophys.ps <- (postdraws$agno.biophys.ps * attributes(pres.abs.scl$agno.biophys.ps)[[3]])+attributes(pres.abs.scl$agno.biophys.ps)[[2]]
+
+# Plotting Dist 2 PA's:
+plot.df <- postdraws %>% 
+  mutate_at(., vars(human.dens.ps), as.factor) %>% 
+  group_by(agno.biophys.ps, human.dens.ps) %>% 
+  summarise(., mean = mean(.epred),
+            lo = quantile(.epred, 0.2),
+            hi = quantile(.epred, 0.8))
+
+levels(plot.df$human.dens.ps) <-  c("Lower 10%", "Mean", "Upper 10%")
+agno.biophys.plot <- ggplot(data=plot.df) +
+  geom_line(aes(x = agno.biophys.ps, y = mean, colour =human.dens.ps), lwd=1.5) +
+  geom_ribbon(aes(ymin=lo, ymax=hi, x=agno.biophys.ps, fill = human.dens.ps), alpha = 0.2) +
+  scale_colour_viridis(discrete = "TRUE", option="D","Population Density")+
+  scale_fill_viridis(discrete = "TRUE", option="D", "Population Density") +
+  ylab("Probability of Conflict") + 
+  xlab("Species Agnostic Biophysical Connectivity")+
+  # guides(fill=guide_legend(title="Population Density"))+
+  theme(text=element_text(size=12,  family="Times New Roman"), legend.text = element_text(size=10),panel.background = element_rect(fill = "white", colour = "grey50"))
+saveRDS(agno.biophys.plot, "data/processed/agno_bio_mixe_plot.rds")
+
+
+p.all <- animal.dens.plot + ground.dens.plot + ndvi.plot + dist2pa.plot + gHM.plot + agno.biophys.plot + plot_annotation(tag_levels = 'a', tag_suffix = ")") +  plot_layout(guides = 'collect')
+
+saveRDS(p.all, "data/processed/general_conflict_all_pred_plot.rds")
 
