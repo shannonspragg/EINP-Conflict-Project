35cc0ada115280c5a793d1ee28aa4ae987033e3f
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-12-01 15:03:43 -0700

    updating, new script

--- black bear analyses/2.  b bear habitat model scripts/4. step selection rsf.R
+++ black bear analyses/2.  b bear habitat model scripts/4. step selection rsf.R
@@ -10,11 +10,11 @@ library(sf)
 library(terra)
 
 # Bring in data: ----------------------------------------------------------
-  # Landcover:
+# Landcover:
 land <- raster("data/processed/bhb_landcover.tif") # TRY WITH AB raster (not cropped)
 projection(land)
 
-  # Bear collar data:
+# Bear collar data:
 bears <- readOGR("data/processed/bear_collar_data.shp")
 projection(bears)
 
@@ -35,15 +35,6 @@ points(bears, col= bears$animlID)
 
 
 # Reclassify landcover data for analysis: ---------------------------------
-classification <- read.table("data/original/landcover reclass.txt", header=T)
-head(classification)
-
-levels(classification$Description2) # look at new classification levels
-
-# convert to matrix to reclassify
-class <- as.matrix(classification[,c(1,3)])
-land_sub <- reclassify(land, rcl = class)
-
 # create layers that represent continuous key land cover types w/ moving window
 forests<-land
 values(forests) <- 0
@@ -64,82 +55,6 @@ layers <- stack(land, forest.focal, shrubgrass.focal)
 names(layers) <- c( "landcover", "forested", "shrub/grass") 
 plot (layers)
 
-
-# Point Selection Function ------------------------------------------------
-install.packages("reshape2")
-library(reshape2)
-
-use <- raster::extract(layers, bears)
-use$BearID <- as.factor(bears$animlID)
-
-#use reshape2, dcast function:
-useBearID <- dcast(use, BearID ~ land, length, value.var = "BearID")
-
-newclass.names <- unique(classification[,3:4])
-names(useCatID) <- c("CatID", as.character(newclass.names[l:13,2]))
-
-# Generate random points and extract landcover categories:
-
-#use sampleRandomfunction from raster to create availability 
-set.seed (8)
-rand.II <- sampleRandom(land, size = 1000) 
-rand.II.land <- data.frame(rand.II)
-#s u m u μ c o u n c s o f e a c h l a n d c o v e r t y p e 
-table(rand.II.land)
-
-#sum up counts of each landcover type
-avail.II <- tapply(rand.II.land, rand.II.land, length) 
-names(avail.II) <- as.characcer(newclass.names[1:14, 2] )
-avail.II
-#remove exotics, which was not observed in sample of use
-avai1.II <-avai1.II[c(-14)]
-
-
-# Running an MCP: ---------------------------------------------------------
-library(sp)
-install.packages("adehabitatHS")
-library(adehabitatHS)
-
-bear.unique <- unique(bears$animlID)
-samples <- 200
-rand.III <- matrix(nrow=0, ncol = 2)
-
-# loop for all individuals
-for(i in 1:length(bear.unique)) {
-  id.i <- bear.unique[i]
-  bear.i <- bears[bears$animlID == id.i,]
-  mcp.i <- mcp(SpatialPoints(coordinates(bear.i)), percent = 99)
-  rand.i <- spsample(mcp.i, type = "random", n= samples)
-  rand.i.sample <- raster::extract(land, rand.i)
-  
-  # make matrix of id and rand samples
-  bear.i <- rep(bear.unique[i], length(rand.i))
-  rand.bear.i <- cbind(bear.i, rand.i.sample)
-  rand.III <- rbind(rand.III, rand.bear.i)
-}
-
-# reshape data with dcast function:
-rand.III <- data.frame(rand.III)
-rand.III$bear.i <- as.factor(rand.III$bear.i)
-colnames(rand.III) = c("bear.i", "land") 
-avail.III <- dcast(rand.III,bear.i ~ land, length, value.var = "bear.i")
-#names(avail.III) <- c( "BearID", as.character(newclass.names[1:13,2])) 
-avail.III
-
-# Calculate selection ratios
-library(adehabitatHS)
-
-sel.ratioII <- widesII(u = useBearID[,c(2:ncol(useBearID))],
-                       a = as.vector(avail.II),
-                       avknown = F, alpha = 0.05)
-summary(sel.ratioII)
-sel.ratioII
-sel.ratioII$wi # selection ratios
-sel.ratioII$se.wi # selection ratio SEs
-plot(sel.ratioII)
-
-
-
 # Step Selection Functions ------------------------------------------------
 # Split bear datetime col into two:
 library(dplyr)
@@ -149,31 +64,15 @@ library(tidyverse)
 bears.sf <- st_as_sf(bears)
 bears.sf$Date <- str_sub(bears.sf$DateTim, 1, 10)
 bears.sf$Time <- str_sub(bears.sf$DateTim, 12, 20)
-
-#bears.sf <- bears.sf %>% tidyr::separate(DateTim, c('Date', 'Time'), sep = " ")
 bears <- as(bears.sf, "Spatial")
 
 # Prep trajectory:
 install.packages("adehabitatLT")
 library(adehabitatLT)
 
-#reformat date ro a POSIXct object for date:
-# substrRight <- function(x, n) {
-#   substr(x, nchar(x) - n+1, nchar(x))
-# }
-
-# bears$Date <- as.character(bears$Date)
-# bear.date <- as.numeric(substrRight(bears$Date, 3))
-# #bear.date <- as.numeric(bears$Date)
-# bears$Date <- as.Date(bear.date, origin = as.Date("2021-08-11 1:00:01"))
-# head(bears)
-# # create POSIXct object:
-# bears$Date <- as.POSIXct(bears$Date, "%Y-%m-%d %H:%M:%S")
-# bear.ltrag <- as.ltraj(xy = coordinates(bears), date = bears$Date, id = bears$animlID, typeII = T)
-
 bears.ltraj <- as.ltraj(xy = coordinates(bears), 
-                          date =  as.POSIXct(paste(bears$Date, bears$Time, sep = " ")), 
-                          id = bears$animlID)
+                        date =  as.POSIXct(paste(bears$Date, bears$Time, sep = " ")), 
+                        id = bears$animlID)
 plot(bears.ltraj, id = "U01")
 
 # distance for first bear id:
@@ -181,7 +80,7 @@ bears.ltraj[[1]][,6]
 hist(bears.ltraj[[1]][,6], main = "First BearID")
 
 #plots of relative movement angles for second CatID 
-#relativeangles:changeindireccionfromprevious timestep 
+#relative angles:change ind ireccion from previous time step 
 rose.diag(na.omit(bears.ltraj[[1]][,10]), bins=12, prop= 1.5) 
 circ.plot(bears.ltraj[[2]][,10], pch = 1)
 
@@ -200,7 +99,7 @@ traj.df <- ld(bears.ltraj)
 avail.dist <- matrix(sample(na.omit(traj.df$dist), 
                             size = n.avail, replace = T), ncol = 10)
 avail.angle <- matrix(sample(na.omit(traj.df$rel.angle), 
-                            size = n.avail, replace = T), ncol = 10)
+                             size = n.avail, replace = T), ncol = 10)
 #name cols:
 colnames(avail.dist) <- c("a.dist1", "a.dist2", "a.dist3", "a.dist4","a.dist5","a.dist6","a.dist7","a.dist8","a.dist9","a.dist10")
 colnames(avail.angle) <- c("a.angle1", "a.angle2", "a.angle3", "a.angle4", "a.angle5", "a.angle6", "a.angle7", "a.angle8", "a.angle9", "a.angle10")
@@ -218,8 +117,8 @@ traj.df[3, c("x", "y")]
 traj.df$abs.angle_t_1 <- NA 
 for(i in 2:nrow(traj.df)) {
   traj.df$abs.angle_t_1[i] <- ifelse(traj.df$id[i] ==
-  traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
-  }
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
 traj.df$abs.angle_t_2 <- NA 
 for(i in 2:nrow(traj.df)) {
   traj.df$abs.angle_t_2[i] <- ifelse(traj.df$id[i] ==
@@ -230,9 +129,43 @@ for(i in 2:nrow(traj.df)) {
   traj.df$abs.angle_t_3[i] <- ifelse(traj.df$id[i] ==
                                        traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
 }
-
+traj.df$abs.angle_t_4 <- NA 
+for(i in 2:nrow(traj.df)) {
+  traj.df$abs.angle_t_4[i] <- ifelse(traj.df$id[i] ==
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
+traj.df$abs.angle_t_5 <- NA 
+for(i in 2:nrow(traj.df)) {
+  traj.df$abs.angle_t_5[i] <- ifelse(traj.df$id[i] ==
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
+traj.df$abs.angle_t_6 <- NA 
+for(i in 2:nrow(traj.df)) {
+  traj.df$abs.angle_t_6[i] <- ifelse(traj.df$id[i] ==
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
+traj.df$abs.angle_t_7 <- NA 
+for(i in 2:nrow(traj.df)) {
+  traj.df$abs.angle_t_7[i] <- ifelse(traj.df$id[i] ==
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
+traj.df$abs.angle_t_8 <- NA 
+for(i in 2:nrow(traj.df)) {
+  traj.df$abs.angle_t_8[i] <- ifelse(traj.df$id[i] ==
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
+traj.df$abs.angle_t_9 <- NA 
+for(i in 2:nrow(traj.df)) {
+  traj.df$abs.angle_t_9[i] <- ifelse(traj.df$id[i] ==
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
+traj.df$abs.angle_t_10 <- NA 
+for(i in 2:nrow(traj.df)) {
+  traj.df$abs.angle_t_10[i] <- ifelse(traj.df$id[i] ==
+                                       traj.df$id[i - 1], traj.df$abs.angle[i - 1] , NA) 
+}
 # calc new coords using trig
-  # use coords for t + 1
+# use coords for t + 1
 traj.df$x_t1 <- traj.df[, "x"] + traj.df[,"dist"] * cos(traj.df[, "abs.angle"]) 
 traj.df$y_t1 <- traj.df[, "y"] + traj.df[, "dist"] * sin(traj.df[,"abs.angle"])
 
@@ -246,6 +179,27 @@ traj.df$y_a2 <- traj.df[, "y"] + traj.df[, "a.dist2"] * sin(traj.df[, "abs.angle
 traj.df$x_a3 <- traj.df[, "x"] + traj.df[, "a.dist3"] * cos(traj.df[, "abs.angle_t_3"] + traj.df[, "a.angle3"]) 
 traj.df$y_a3 <- traj.df[, "y"] + traj.df[, "a.dist3"] * sin(traj.df[, "abs.angle_t_3"] + traj.df[, "a.angle3"])
 
+traj.df$x_a4 <- traj.df[, "x"] + traj.df[, "a.dist4"] * cos(traj.df[, "abs.angle_t_4"] + traj.df[, "a.angle4"]) 
+traj.df$y_a4 <- traj.df[, "y"] + traj.df[, "a.dist4"] * sin(traj.df[, "abs.angle_t_4"] + traj.df[, "a.angle4"])
+
+traj.df$x_a5 <- traj.df[, "x"] + traj.df[, "a.dist5"] * cos(traj.df[, "abs.angle_t_5"] + traj.df[, "a.angle5"]) 
+traj.df$y_a5 <- traj.df[, "y"] + traj.df[, "a.dist5"] * sin(traj.df[, "abs.angle_t_5"] + traj.df[, "a.angle5"])
+
+traj.df$x_a6 <- traj.df[, "x"] + traj.df[, "a.dist6"] * cos(traj.df[, "abs.angle_t_6"] + traj.df[, "a.angle6"]) 
+traj.df$y_a6 <- traj.df[, "y"] + traj.df[, "a.dist6"] * sin(traj.df[, "abs.angle_t_6"] + traj.df[, "a.angle6"])
+
+traj.df$x_a7 <- traj.df[, "x"] + traj.df[, "a.dist7"] * cos(traj.df[, "abs.angle_t_7"] + traj.df[, "a.angle7"]) 
+traj.df$y_a7 <- traj.df[, "y"] + traj.df[, "a.dist7"] * sin(traj.df[, "abs.angle_t_7"] + traj.df[, "a.angle7"])
+
+traj.df$x_a8 <- traj.df[, "x"] + traj.df[, "a.dist8"] * cos(traj.df[, "abs.angle_t_8"] + traj.df[, "a.angle8"]) 
+traj.df$y_a8 <- traj.df[, "y"] + traj.df[, "a.dist8"] * sin(traj.df[, "abs.angle_t_8"] + traj.df[, "a.angle8"])
+
+traj.df$x_a9 <- traj.df[, "x"] + traj.df[, "a.dist9"] * cos(traj.df[, "abs.angle_t_9"] + traj.df[, "a.angle9"]) 
+traj.df$y_a9 <- traj.df[, "y"] + traj.df[, "a.dist9"] * sin(traj.df[, "abs.angle_t_9"] + traj.df[, "a.angle9"])
+
+traj.df$x_a10 <- traj.df[, "x"] + traj.df[, "a.dist10"] * cos(traj.df[, "abs.angle_t_10"] + traj.df[, "a.angle10"]) 
+traj.df$y_a10 <- traj.df[, "y"] + traj.df[, "a.dist10"] * sin(traj.df[, "abs.angle_t_10"] + traj.df[, "a.angle10"])
+
 # reformat data for step selection:
 traj.df <- traj.df[complete.cases(traj.df),] #remove NAs
 traj.use <- data.frame(use = rep(1, nrow(traj.df)),
@@ -256,20 +210,42 @@ traj.a2 <- data.frame(use = rep(0, nrow(traj.df)),
                       traj.df[,c("id", "pkey", "date", "x_a2", "y_a2")])
 traj.a3 <- data.frame(use = rep(0, nrow(traj.df)),
                       traj.df[,c("id", "pkey", "date", "x_a3", "y_a3")])
+traj.a4 <- data.frame(use = rep(0, nrow(traj.df)),
+                      traj.df[,c("id", "pkey", "date", "x_a4", "y_a4")])
+traj.a5 <- data.frame(use = rep(0, nrow(traj.df)),
+                      traj.df[,c("id", "pkey", "date", "x_a5", "y_a5")])
+traj.a6 <- data.frame(use = rep(0, nrow(traj.df)),
+                      traj.df[,c("id", "pkey", "date", "x_a6", "y_a6")])
+traj.a7 <- data.frame(use = rep(0, nrow(traj.df)),
+                      traj.df[,c("id", "pkey", "date", "x_a7", "y_a7")])
+traj.a8 <- data.frame(use = rep(0, nrow(traj.df)),
+                      traj.df[,c("id", "pkey", "date", "x_a8", "y_a8")])
+traj.a9 <- data.frame(use = rep(0, nrow(traj.df)),
+                      traj.df[,c("id", "pkey", "date", "x_a9", "y_a9")])
+traj.a10 <- data.frame(use = rep(0, nrow(traj.df)),
+                      traj.df[,c("id", "pkey", "date", "x_a10", "y_a10")])
+
 names(traj.use) <- c("use", "id", "pair", "date", "x", "y")
 names(traj.a1) <- c("use", "id", "pair", "date", "x", "y")
 names(traj.a2) <- c("use", "id", "pair", "date", "x", "y")
 names(traj.a3) <- c("use", "id", "pair", "date", "x", "y")
+names(traj.a4) <- c("use", "id", "pair", "date", "x", "y")
+names(traj.a5) <- c("use", "id", "pair", "date", "x", "y")
+names(traj.a6) <- c("use", "id", "pair", "date", "x", "y")
+names(traj.a7) <- c("use", "id", "pair", "date", "x", "y")
+names(traj.a8) <- c("use", "id", "pair", "date", "x", "y")
+names(traj.a9) <- c("use", "id", "pair", "date", "x", "y")
+names(traj.a10) <- c("use", "id", "pair", "date", "x", "y")
 
 # append use and availible data together: (traj.a4-10 should be created in same way as above)
-stepdata.final <- rbind(traj.use, traj.a1, traj.a2, traj.a3)
+stepdata.final <- rbind(traj.use, traj.a1, traj.a2, traj.a3, traj.a4, traj.a5, traj.a6, traj.a7, traj.a8, traj.a9, traj.a10)
 
 # use extract function to get info on environmental covs betweewn paired use and availability locations
-  # create spdf
+# create spdf
 step.coords <- SpatialPoints(stepdata.final[,c("x", "y")], proj4string = CRS("+proj=utm +zone=12 +datum=NAD83 +units=m +no_defs"))
-  # extract covariates from layers
+# extract covariates from layers
 cov <- raster::extract(layers, step.coords)
-  # add covs to dataframe of use/availible:
+# add covs to dataframe of use/availible:
 stepdata.final <- data.frame(cbind(stepdata.final, cov))
 
 # make landcover categories:
@@ -292,16 +268,16 @@ stepdata.final <- stepdata.final %>%
 install.packages("survival")
 library(survival)
 
-  # conditional logistic
+# conditional logistic
 logit.ssf <- clogit(use ~ forested + shrub.grass + landcover.desc + strata(pair), data = stepdata.final)
 
-  # including bearID as cluster:
+# including bearID as cluster:
 logit.bear.ssf <- clogit(use ~ forested + shrub.grass + landcover.desc + strata(pair) + cluster(id), method = "approximate", data = stepdata.final)
 
 # logistic ignoring local pairing structure of data
 logit.rsf <- glm(use ~ forested + shrub.grass + landcover.desc, family = "binomial", data = stepdata.final)
 
-  # compare coefficients
+# compare coefficients
 logit.ssf
 coef(logit.ssf)
 logit.bear.ssf
