f2923499c83b14e2b6df7d398d5d602fbf0755b7
Shannon Spragg <shannonspragg@u.boisestate.edu>
2022-12-06 14:30:36 -0700

    adding in compensation claims

--- black bear analyses/4.  b bear conflict analysis scripts/1. data download.R
+++ black bear analyses/4.  b bear conflict analysis scripts/1. data download.R
@@ -20,6 +20,14 @@ gdrive_files <- drive_ls(folder)
 lapply(gdrive_files$id, function(x) drive_download(as_id(x),
                                                    path = paste0(here::here("data/original/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
 
+# Predator Compensations reports:
+folder_url <- "https://drive.google.com/drive/u/0/folders/18GaXCxqceCKoAsB9wWdPuj-OJKDIX2Gc" # compensation data
+folder <- drive_get(as_id(folder_url))
+gdrive_files <- drive_ls(folder)
+#have to treat the gdb as a folder and download it into a gdb directory in order to deal with the fact that gdb is multiple, linked files
+lapply(gdrive_files$id, function(x) drive_download(as_id(x),
+                                                   path = paste0(here::here("data/original/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
+
 # Road Collision reports:
 folder_url <- "https://drive.google.com/drive/folders/1aaITDOTD42WmuKx9Xuhr7Yp3-EA-I-6O" # collision data
 folder <- drive_get(as_id(folder_url))
--- black bear analyses/4.  b bear conflict analysis scripts/3. add IEM data.R
+++ black bear analyses/4.  b bear conflict analysis scripts/3. add IEM data.R
@@ -17,11 +17,12 @@ library(raster)
 # Bring in our  Data --------------------------------------------
 
 iem.reports <- read.csv("data/original/IEM Download_2022-10-01 .csv") # these are all black bear reports
+comp.reports <- read.csv("data/original/willdife_predator_compensations.csv")
 prov.conflict <- st_read("data/processed/conflict_confirmed_dataframe.shp")
 bhb.50k.buf <- st_read("data/processed/bhb_50km.shp")
 bhw <- st_read("data/original/BHB_Subwatershed_Boundary.shp")
 head(iem.reports)
-
+head(comp.reports)
 
 # Make IEM spatial --------------------------------------------------------
 
@@ -37,10 +38,38 @@ xy$X.5 <- as.numeric(xy$X.5)
 
 iem.spdf<-SpatialPointsDataFrame(coords = xy,data = iem.df,
                                       proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+# Now compensation reports:
+comp.reports <- comp.reports %>% 
+  mutate(comp.reports, bears = if_else(OCC_SPECIES == "BLACK BEAR", 1, 0)) %>%
+  mutate(comp.reports, wolves = if_else(OCC_SPECIES == "WOLF", 1, 0)) %>%
+  mutate(comp.reports, cougars = if_else(OCC_SPECIES == "COUGAR", 1, 0))
+
+xy2<-comp.reports[,c(6,5)]
+comp.spdf<-SpatialPointsDataFrame(coords = xy2,data = comp.reports,
+                                 proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+
 
 # Ensure this is a sf data frame:
 # conflict.data.sf <- as(conflict.spdf, "sf")
 iem.data.sf <- st_as_sf(iem.spdf)
+
+comp.sf <- st_as_sf(comp.spdf)
+
+comp.sf <- comp.sf %>% 
+  dplyr::select(., c('OCC_FILE_NUMBER', 'OCCURRENCE_TYPE_DESC', 'ACTION_TYPE_DESCRIPTION', 'OCC_CITY', 'OCC_POSTAL_CODE', 'OCC_WMU_CODE', 'OCC_SPECIES',
+                     'OCC_NUMBER_ANIMALS', 'OCC_PRIMARY_ATTRACTANT', 'OCC_VALIDITY_INFORMATION', 'bears', 'wolves', 'cougars', 'geometry', 'OCC_OCCURRENCE_TMST', 'SITE_NAME'))
+
+comp.sf <- mutate(comp.sf, id = row_number())
+comp.sf <- comp.sf %>%           # Reorder data frame
+  dplyr::select("id", everything())
+comp.sf$id <- as.numeric(comp.sf$id)
+
+# Crop reports down to BHB watershed:
+st_crs(conflict.dataset.conf) == st_crs(bhb.50k.buf) #FALSE
+conflict.reproj <- st_transform(conflict.dataset.conf, st_crs(bhb.50k.buf))
+st_crs(conflict.reproj) == st_crs(bhb.50k.buf) #TRUE
+
+conflict.bhb.50k.buf <- st_intersection(conflict.reproj, bhb.50k.buf) # This gives 2057 total reports
 
 head(iem.data.sf)
 unique(iem.data.sf$X.1)
@@ -89,23 +118,45 @@ IEM.filt['AREA_HA'] <- NA
 # Reorder the columns to match:
 IEM.sf <- IEM.filt[ , c(1,7,4,8,9,10,11,5,12,13,14,3,2,15,16,17,18,6)]
 
+### Now adjust comp data cols:
+comp.sf <- rename(comp.sf, OCC_FIL = OCC_FILE_NUMBER)
+comp.sf <- rename(comp.sf, OCCURRE = OCCURRENCE_TYPE_DESC)
+comp.sf <- rename(comp.sf, ACTION_ = ACTION_TYPE_DESCRIPTION)
+comp.sf <- rename(comp.sf, OCC_CIT = OCC_CITY)
+comp.sf <- rename(comp.sf, OCC_POS = OCC_POSTAL_CODE)
+comp.sf <- rename(comp.sf, OCC_WMU = OCC_WMU_CODE)
+comp.sf <- rename(comp.sf, OCC_SPE = OCC_SPECIES)
+comp.sf <- rename(comp.sf, OCC_NUM = OCC_NUMBER_ANIMALS)
+comp.sf <- rename(comp.sf, OCC_PRI = OCC_PRIMARY_ATTRACTANT)
+comp.sf <- rename(comp.sf, OCC_VAL = OCC_VALIDITY_INFORMATION)
+comp.sf <- rename(comp.sf, OCC_OCC = OCC_OCCURRENCE_TMST)
+comp.sf <- rename(comp.sf, SITE_NA = SITE_NAME)
+comp.sf['AREA_HA'] <- NA
+
+comp.sf <- comp.sf[, c(1,2,3,4,5,6,7,8,9,10,11,15,16,12,13,14,18,17)]
+
 # Assign CCS regions: -----------------------------------------------------
 
 # Write this as a .shp for later:
 ab.ccs <- st_read("data/processed/AB_CCS.shp")
 ab.ccs.reproj <- st_transform(ab.ccs, st_crs(bhb.50k.buf))
 iem.reproj <- st_transform(IEM.sf, st_crs(bhb.50k.buf))
+comp.reproj <- st_transform(comp.sf, st_crs(bhb.50k.buf))
 
 # Assign our points to a CCS category:
 iem.ccs.join <- st_join(iem.reproj, left = TRUE, ab.ccs.reproj) # join points
+comp.ccs.join <- st_join(comp.reproj, left = TRUE, ab.ccs.reproj) # join points
 
 head(iem.ccs.join) # Assigned points to a CCS category
 
 iem.ccs.join <- iem.ccs.join %>% 
   dplyr::select(., -c(22,21,19))
 
+comp.ccs.join <- comp.ccs.join %>% 
+  dplyr::select(., -c(22,21,19))
+
 # Join our reports together:
-conf.conflict.all <- rbind(prov.conflict, iem.ccs.join) # now we have 1128 obs!
+conf.conflict.all <- rbind(prov.conflict, iem.ccs.join, comp.ccs.join) # now we have 1130 obs!
 
 # Add a column for conflict presence:
 conf.conflict.all$conflict_pres <- 1
