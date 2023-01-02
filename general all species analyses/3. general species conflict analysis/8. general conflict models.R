8a8d7860999fa06327dba83b5e528f9f946e997b
Shannon Spragg <shannonspragg@u.boisestate.edu>
2023-01-02 16:15:07 -0700

    RE-RUNNING

--- general all species analyses/3. general species conflict analysis/8. general conflict models.R
+++ general all species analyses/3. general species conflict analysis/8. general conflict models.R
@@ -24,33 +24,33 @@ conf.pres.abs <- st_read(here::here("./Data/processed/pres_abs_full_df.shp")) %>
 
 #filter some of the absences
 pres.abs.filter <- conf.pres.abs %>% 
-  dplyr::select(., c(cnflct_, dst2p_k, hum_dns, anml_fr, grnd_cr, ndvi, gHM, agn_bph, gfcl_bp, frst_s_, CCSNAME)) 
+  dplyr::select(., c(cnflct_, dst2p_k, hum_dns, anml_fr, grnd_cr, ndvi, gHM, agn_bph, gfcl_bp, CCSNAME)) 
 
 colnames(pres.abs.filter) <- c("conflict_presence_ps", "dist.2.pa.ps", "human.dens.ps", "animal.farm.dens.ps", "ground.crop.dens.ps", "ndvi.ps", 
-                               "gHM.ps", "agno.biophys.ps", "gen.focal.biophys.ps", "forest.sp.biophys.ps",  "CCSNAME.ps")
+                               "gHM.ps", "agno.biophys.ps", "gen.focal.biophys.ps",  "CCSNAME.ps")
 
 
 # Scale Data for Analysis: ------------------------------------------------
 ## Here we scale the predictors for analysis
 pres.abs.scl <- pres.abs.filter %>% 
   mutate_at(c("dist.2.pa.ps", "human.dens.ps", "animal.farm.dens.ps", "ground.crop.dens.ps", "ndvi.ps", 
-              "gHM.ps", "agno.biophys.ps", "gen.focal.biophys.ps", "forest.sp.biophys.ps"), scale)
+              "gHM.ps", "agno.biophys.ps", "gen.focal.biophys.ps"), scale)
 
 saveRDS(pres.abs.scl, "data/processed/pres_abs_scl.rds")
 
 # Run General Conflict Models: --------------------------------------------
 t_prior <- student_t(df = 7, location = 0, scale = 1.5)
-SEED<-14124869
+SEED <- 14124869
 
 # Full Model:
-post.pa.full <- stan_glmer(conflict_presence_ps ~ dist.2.pa.ps + human.dens.ps + animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + (1 | CCSNAME.ps), 
+post.pa.full <- stan_glmer(conflict_presence_ps ~ dist.2.pa.ps + human.dens.ps + animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + gen.focal.biophys.ps + (1 | CCSNAME.ps), 
                            data = pres.abs.scl,
                            family = binomial(link = "logit"), # define our binomial glm
                            prior = t_prior, prior_intercept = t_prior, QR=TRUE,
                            iter = 3000, chains=5,
                            seed = SEED) # we add seed for reproducibility
 # Partial Model:
-post.pa.partial <- stan_glmer(conflict_presence_ps ~ animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + (1 | CCSNAME.ps), 
+post.pa.partial <- stan_glmer(conflict_presence_ps ~ animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + gen.focal.biophys.ps + (1 | CCSNAME.ps), 
                            data = pres.abs.scl,
                            family = binomial(link = "logit"), # define our binomial glm
                            prior = t_prior, prior_intercept = t_prior, QR=TRUE,
@@ -58,17 +58,11 @@ post.pa.partial <- stan_glmer(conflict_presence_ps ~ animal.farm.dens.ps + groun
                            seed = SEED) # we add seed for reproducibility
 
 # Full Model + Quadratic for Pop Dens:
-post.pa.full.quad <- update(post.pa.full, formula = conflict_presence_ps ~ I(human.dens.ps^2) + dist.2.pa.ps + animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + (1 | CCSNAME.ps), QR = TRUE)
+post.pa.full.quad <- update(post.pa.full, formula = conflict_presence_ps ~ I(human.dens.ps^2) + dist.2.pa.ps + animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + gen.focal.biophys.ps + (1 | CCSNAME.ps), QR = TRUE)
 
 # Intercept-only model:
 post.int.only <-  update(post.pa.full, formula = conflict_presence_ps ~ 1+ (1 | CCSNAME.ps), QR = FALSE)
 
-post.int.only <- stan_glmer(conflict_presence_ps ~ 1 + (1 | CCSNAME.ps), 
-                           data = pres.abs.scl,
-                           family = binomial(link = "logit"), # define our binomial glm
-                           prior = t_prior, prior_intercept = t_prior, QR=FALSE,
-                           iter = 3000, chains=5,
-                           seed = SEED) # we add seed for reproducibility
 
 saveRDS(post.pa.full, "data/processed/post_pa_full.rds")
 saveRDS(post.pa.partial, "data/processed/post_pa_partial.rds")
