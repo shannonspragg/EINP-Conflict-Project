6bb924271ac8cd8cabe968a91c923985e54269d4
Shannon Spragg <shannonspragg@u.boisestate.edu>
2023-01-02 14:06:11 -0700

    updating gen conf scripts

---
+++ general all species analyses/3. general species conflict analysis/6.  dowload connectivity outputs.R
@@ -0,0 +1,74 @@
+
+# Dowload Biophysical Connectivity Models: --------------------------------
+
+### Here we download our biophysical connectivity outputs for the general conflict analysis
+
+# Load Packages -------------------------------------------------------
+library(googledrive)
+library(tidyverse)
+
+# Load our Data with GoogleDrive: -----------------------------------------
+options(
+  gargle_oauth_cache = ".secrets",
+  gargle_oauth_email = TRUE
+)
+
+# Biophysical Connectivity Models:
+folder_url <- "https://drive.google.com/drive/u/0/folders/1Duzc_0Ed-YsdQgpNq0SW7flPpPXewPx9" # biophys outputs 
+folder <- drive_get(as_id(folder_url))
+gdrive_files <- drive_ls(folder)
+#have to treat the gdb as a folder and download it into a gdb directory in order to deal with the fact that gdb is multiple, linked files
+lapply(gdrive_files$id, function(x) drive_download(as_id(x),
+                                                   path = paste0(here::here("data/processed/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
+
+# Let's crop these to the BHW boundary and 50km buffer --------------------
+library(terra)
+
+# Bring in covariate data: -------------------------------------------------------------
+bhb.50km.boundary <- st_read("data/processed/bhb_50km.shp")
+bhb.watershed <- st_read("data/original/BHB_Subwatershed_Boundary.shp")
+temp.rast <- rast("data/processed/dist2pa_km_bhb.tif")
+
+sp_agnostic_cumcurr <- rast("data/processed/agnostic_cum_currmap.tif")
+sp_agnostic_norm <- rast("data/processed/agnostic_normalized_cum_currmap.tif")
+
+gen_focal_cumcurr <- rast("data/processed/general_focal_cum_currmap.tif")
+gen_focal_norm <- rast("data/processed/general_focal_normalized_cum_currmap.tif")
+
+
+# Make these vectors:
+bhb.50km.v <- vect(bhb.50km.boundary)
+bhw.v <- vect(bhb.watershed)
+
+# Mask layers to the BHW buffer and boundary line -------------------------
+
+# Crop our rasters to the BH watershed 50km buffer shape:
+
+agno_cumcurr.bhb <- terra::mask(sp_agnostic_cumcurr, bhb.50km.v)
+agno_norm.bhb <- terra::mask(sp_agnostic_norm, bhb.50km.v)
+
+gfocal_cumcurr.bhb <- terra::mask(gen_focal_cumcurr, bhb.50km.v)
+gfocal_norm.bhb <- terra::mask(gen_focal_norm, bhb.50km.v)
+
+# Crop to BHW boundary:
+agno_cumcurr.bhw <- terra::mask(sp_agnostic_cumcurr, bhw.v)
+agno_norm.bhw <- terra::mask(sp_agnostic_norm, bhw.v)
+
+gfocal_cumcurr.bhw <- terra::mask(gen_focal_cumcurr, bhw.v)
+gfocal_norm.bhw <- terra::mask(gen_focal_norm, bhw.v)
+
+# Variables with 50km buffer of BHW:
+writeRaster(agno_cumcurr.bhb, "data/processed/bhw_agno_cumcurr_50km.tif", overwrite=TRUE)
+writeRaster(agno_norm.bhb, "data/processed/bhw_agno_norm_50km.tif", overwrite=TRUE)
+
+writeRaster(gfocal_cumcurr.bhb, "data/processed/bhw_gfocal_cumcurr_50km.tif", overwrite=TRUE)
+writeRaster(gfocal_norm.bhb, "data/processed/bhw_gfocal_norm_50km.tif", overwrite=TRUE)
+
+
+# Variables with boundary of BHW:
+writeRaster(agno_cumcurr.bhw, "data/processed/bhw_agno_cumcurr.tif", overwrite=TRUE)
+writeRaster(agno_norm.bhw, "data/processed/bhw_agno_norm.tif", overwrite=TRUE)
+
+writeRaster(gfocal_cumcurr.bhw, "data/processed/bhw_gfocal_cumcurr.tif", overwrite=TRUE)
+writeRaster(gfocal_norm.bhw, "data/processed/bhw_gfocal_norm.tif", overwrite=TRUE)
+
---
+++ general all species analyses/3. general species conflict analysis/8. general conflict models.R
@@ -0,0 +1,76 @@
+
+# General Conflict Model --------------------------------------------------
+## Here we run our general conflict full and null models. 
+
+
+# Load Packages: ----------------------------------------------------------
+library(rstanarm)
+library(tidyverse)
+library(sf)
+options(mc.cores = parallel::detectCores())
+library(loo)
+library(bayesplot)
+library(tidybayes)
+library(ggeffects)
+library(viridis)
+library(patchwork)
+library(modelr)
+theme_set(bayesplot::theme_default(base_family = "sans"))
+
+
+# Bring in Data: ----------------------------------------------------------
+conf.pres.abs <- st_read(here::here("./Data/processed/pres_abs_full_df.shp")) %>% st_drop_geometry()
+
+
+#filter some of the absences
+pres.abs.filter <- conf.pres.abs %>% 
+  dplyr::select(., c(cnflct_, dst2p_k, hum_dns, anml_fr, grnd_cr, ndvi, gHM, agn_bph, gfcl_bp, frst_s_, CCSNAME)) 
+
+colnames(pres.abs.filter) <- c("conflict_presence_ps", "dist.2.pa.ps", "human.dens.ps", "animal.farm.dens.ps", "ground.crop.dens.ps", "ndvi.ps", 
+                               "gHM.ps", "agno.biophys.ps", "gen.focal.biophys.ps", "forest.sp.biophys.ps",  "CCSNAME.ps")
+
+
+# Scale Data for Analysis: ------------------------------------------------
+## Here we scale the predictors for analysis
+pres.abs.scl <- pres.abs.filter %>% 
+  mutate_at(c("dist.2.pa.ps", "human.dens.ps", "animal.farm.dens.ps", "ground.crop.dens.ps", "ndvi.ps", 
+              "gHM.ps", "agno.biophys.ps", "gen.focal.biophys.ps", "forest.sp.biophys.ps"), scale)
+
+saveRDS(pres.abs.scl, "data/processed/pres_abs_scl.rds")
+
+# Run General Conflict Models: --------------------------------------------
+t_prior <- student_t(df = 7, location = 0, scale = 1.5)
+SEED<-14124869
+
+# Full Model:
+post.pa.full <- stan_glmer(conflict_presence_ps ~ dist.2.pa.ps + human.dens.ps + animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + (1 | CCSNAME.ps), 
+                           data = pres.abs.scl,
+                           family = binomial(link = "logit"), # define our binomial glm
+                           prior = t_prior, prior_intercept = t_prior, QR=TRUE,
+                           iter = 3000, chains=5,
+                           seed = SEED) # we add seed for reproducibility
+# Partial Model:
+post.pa.partial <- stan_glmer(conflict_presence_ps ~ animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + (1 | CCSNAME.ps), 
+                           data = pres.abs.scl,
+                           family = binomial(link = "logit"), # define our binomial glm
+                           prior = t_prior, prior_intercept = t_prior, QR=TRUE,
+                           iter = 3000, chains=5,
+                           seed = SEED) # we add seed for reproducibility
+
+# Full Model + Quadratic for Pop Dens:
+post.pa.full.quad <- update(post.pa.full, formula = conflict_presence_ps ~ I(human.dens.ps^2) + dist.2.pa.ps + animal.farm.dens.ps + ground.crop.dens.ps + ndvi.ps + gHM.ps + agno.biophys.ps + (1 | CCSNAME.ps), QR = TRUE)
+
+# Intercept-only model:
+post.int.only <-  update(post.pa.full, formula = conflict_presence_ps ~ 1+ (1 | CCSNAME.ps), QR = FALSE)
+
+post.int.only <- stan_glmer(conflict_presence_ps ~ 1 + (1 | CCSNAME.ps), 
+                           data = pres.abs.scl,
+                           family = binomial(link = "logit"), # define our binomial glm
+                           prior = t_prior, prior_intercept = t_prior, QR=FALSE,
+                           iter = 3000, chains=5,
+                           seed = SEED) # we add seed for reproducibility
+
+saveRDS(post.pa.full, "data/processed/post_pa_full.rds")
+saveRDS(post.pa.partial, "data/processed/post_pa_partial.rds")
+saveRDS(post.pa.full.quad, "data/processed/post_pa_full_quad.rds")
+saveRDS(post.int.only, "data/processed/post_int_only.rds")
