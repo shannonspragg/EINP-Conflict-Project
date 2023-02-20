f78476e0b1229af68c219dd534c1aac8ce2f4870
Shannon Spragg <shannonspragg@u.boisestate.edu>
2023-02-20 11:36:08 -0700

    script for stacking connectivity models by weight

---
+++ 5.  carnivore summary analyses/1.  carnivore connectivity/1. synthesized connectivity model.R
@@ -0,0 +1,59 @@
+# Buildling synthesized carnivore connectivity model ----------------------
+  ## Here we seek to merge the three carnivore species-specific connectivity models into one overall model. This can be done 
+  # a few different ways, either through weighted sums (fuzzy sum approach) or by multiplying each surface by it's weighted value and 
+  # then stacking these together.
+
+
+# Load packages -----------------------------------------------------------
+library(raster)
+library(terra)
+library(sf)
+library(tidyverse)
+
+
+# Bring in data -----------------------------------------------------------
+bbear.conf.cumcurr <- rast("data/processed/bbear_conflict_cum_currmap.tif")
+bbear.conf.norm <- rast("data/processed/bbear_conflict_normalized_cum_currmap.tif")
+
+wolf.conf.cumcurr <- rast("data/processed/wolf_conf_cum_currmap.tif")
+wolf.conf.norm <- rast("data/processed/wolf_conf_normalized_cum_currmap.tif")
+
+cougar.conf.cumcurr <- rast("data/processed/cougar_conflict_cum_currmap.tif")
+cougar.conf.norm <- rast("data/processed/cougar_conflict_normalized_cum_currmap.tif")
+
+all.conflict.df <- st_read("data/processed/conflict_conf_comp_dataframe.shp")
+
+
+# Method 1: Multiple across using data proportions ------------------------
+    # Here we will multiply each surface by the proportion of species reports to total reports. Then we will add these weighted surfaces
+    # together into one 
+
+  # Look at counts of each species - this will determine weight:
+sum(all.conflict.df$bears) # 588
+sum(all.conflict.df$wolves) # 60
+sum(all.conflict.df$cougars) # 203
+
+  # Total wildlife reports is 4827, so proportions are as follows:
+  #     black bear - 0.1218
+  #     wolf - 0.0124
+  #     cougar - 0.0421
+
+## Multiply weights to each species connectivity model:
+bbear.cum.adj <- 0.1218 * bbear.conf.cumcurr
+bbear.norm.adj <- 0.1218 * bbear.conf.norm
+
+wolf.cum.adj <- 0.0124 * wolf.conf.cumcurr
+wolf.norm.adj <- 0.0124 * wolf.conf.norm
+
+cougar.cum.adj <- 0.0421 * cougar.conf.cumcurr
+cougar.norm.adj <- 0.0421 * cougar.conf.norm
+
+## Stack to make our two carnivore connectivity models:
+carnivore.cumcurr <- c(bbear.cum.adj, wolf.cum.adj, cougar.cum.adj)
+carnivore.norm <- c(bbear.norm.adj, wolf.norm.adj, cougar.norm.adj)
+
+carnivore.cumcurr.sum <- sum(carnivore.cumcurr)
+carnivore.norm.sum <- sum(carnivore.norm)
+
+
+
